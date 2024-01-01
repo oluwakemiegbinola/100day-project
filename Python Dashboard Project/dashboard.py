117c63cea65a4c898896bc39fb554b307d4c914d
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2024-01-01 16:44:56 +0530

    updated

---
+++ Python Dashboard Project/dashboard.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd 
+import os
+import base64
+import warnings
+warnings.filterwarnings('ignore')
+
+st.set-page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
+
+st.title(" :bar_chart: sample SuperStore EDA")
